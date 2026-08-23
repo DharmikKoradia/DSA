40b834ba84ad718ed7d08c332ea88f3018b38217
DharmikKoradia <dharmik.koradia@gmail.com>
2026-08-23 12:26:47 +0530

    Add The Fibonacci Segment [B] from Codeforces

---
+++ 365/B - The Fibonacci Segment/solution.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+#define PB push_back
+#define F first
+#define S second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+ 
+#define FOR(i,a,b) for(ll i=(a); i<(b); i++)
+#define RFOR(i,a,b) for(ll i=(a); i>=(b); i--)
+ 
+#define YES cout<<"Yes
+"
+#define NO cout<<"No
+"
+#define endl '
+'
+#define DEBUG(x) cerr<<#x<<":"<<x<<"
+";
+ 
+ 
+ll treeOperator(ll a,ll b);
+void buildSegmentTree(vll &tree,vll &nums,ll i,ll l,ll r);
+ll treeQuery(vll &tree,ll start,ll end,ll i,ll l,ll r);
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    
+    vll nums(n);
+    
+    FOR(i,0,n) cin>>nums[i];
+    if(n<=2){
+        cout<<n<<"
+";
+        return;
+    }
+    
+    ll ans=2,l=0,r=2;
+    
+    while(r<n){
+        while(r<n && nums[r]==(nums[r-1]+nums[r-2])) r++;
+        ans = max(ans,r-l);
+        l=r-1;
+        r++;
+    }
+    
+    cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
+ 
+ 
+void buildSegmentTree(vll &tree,vll &nums,ll i,ll l,ll r){
+    if(l==r){
+        tree[i] = nums[l];
+        return;
+    }
+    
+    ll mid = l + ((r-l)>>1);
+    buildSegmentTree(tree,nums,2*i+1,l,mid);
+    buildSegmentTree(tree,nums,2*i+2,mid+1,r);
+    
+    tree[i] = treeOperator(tree[2*i+1],tree[2*i+2]);
+}
+ 
+ll treeOperator(ll a,ll b){
+    return (a&b);    
+}
+ 
+ll treeQuery(vll &tree,ll start,ll end,ll i,ll l,ll r){
+    ll mid = l + ((r-l)>>1);
+    if(start>r || end<l) return 0;
+    else if(l<=start && r<=end) return tree[i];
+    else{
+        return treeOperator(treeQuery(tree,start,end,2*i+1,l,mid),
+            treeQuery(tree,start,end,2*i+2,mid+1,r)
+        );
+    }
+}
