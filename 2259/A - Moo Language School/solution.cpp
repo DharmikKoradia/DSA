671b4482c2fbb98e26f9c947d64b220c69732bd8
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-05 20:20:41 +0530

    Add Moo Language School [A] from Codeforces

---
+++ 2259/A - Moo Language School/solution.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+#define PB push_back
+#define F first
+#define S second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+ 
+#define FOR(i,a,b) for(ll i=(a); i<(b); i++)
+#define RFOR(i,a,b) for(ll i=(a); i>=(b); i--)
+ 
+#define YES cout<<"Yes
+"
+#define NO cout<<"No
+"
+#define endl '
+'
+#define DEBUG(x) cerr<<#x<<":"<<x<<"
+";
+ 
+ 
+ll treeOperator(ll a,ll b);
+void buildSegmentTree(vll &tree,vll &nums,ll i,ll l,ll r);
+ll treeQuery(vll &tree,ll start,ll end,ll i,ll l,ll r);
+ 
+void solve()
+{
+    ll n,k;
+    cin>>n>>k;
+    
+    string s;
+    cin>>s;
+    
+    ll ans=0;
+    for(int i=0;i<n/k;i++){
+        bool zero = true;
+        for(int j=(k*i);j<k*(i+1);j++){
+            if(s[j]=='0'){
+                zero=false;
+                break;
+            }
+        }
+        ans+=zero;
+    }
+    
+    cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
+ 
+ 
+void buildSegmentTree(vll &tree,vll &nums,ll i,ll l,ll r){
+    if(l==r){
+        tree[i] = nums[l];
+        return;
+    }
+    
+    ll mid = l + ((r-l)>>1);
+    buildSegmentTree(tree,nums,2*i+1,l,mid);
+    buildSegmentTree(tree,nums,2*i+2,mid+1,r);
+    
+    tree[i] = treeOperator(tree[2*i+1],tree[2*i+2]);
+}
+ 
+ll treeOperator(ll a,ll b){
+    return (a&b);    
+}
+ 
+ll treeQuery(vll &tree,ll start,ll end,ll i,ll l,ll r){
+    ll mid = l + ((r-l)>>1);
+    if(start>r || end<l) return 0;
+    else if(l<=start && r<=end) return tree[i];
+    else{
+        return treeOperator(treeQuery(tree,start,end,2*i+1,l,mid),
+            treeQuery(tree,start,end,2*i+2,mid+1,r)
+        );
+    }
+}
