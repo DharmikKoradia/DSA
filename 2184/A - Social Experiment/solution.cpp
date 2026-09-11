e77069beb2a71fa1ed6659717e5b0b8dfbe9d2e4
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:37:33 +0530

    Add Social Experiment [A] from Codeforces

---
+++ 2184/A - Social Experiment/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    
+    if(n==2) cout<<"2
+";
+    else if(n==3) cout<<"3
+";
+    else if(n%2) cout<<"1
+";
+    else cout<<"0
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
