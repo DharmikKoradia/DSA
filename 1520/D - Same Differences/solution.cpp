ccfc67adf4070bb489fe86d316d8e18694525732
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:03:52 +0530

    Add Same Differences [D] from Codeforces

---
+++ 1520/D - Same Differences/solution.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define SORT(x) sort(x.begin(),x.end())
+#define RSORT(x) sort(x.rbegin(),x.rend())
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+#define V(x,n) vector<ll> x(n);
+ 
+void solve()
+{
+   ll n,ans=0;
+   cin>>n;
+   V(nums,n);
+   FOR(i,0,n) cin>>nums[i];
+   
+   unordered_map<ll,ll> count;
+   
+   FOR(i,0,n){
+       ll curr = nums[i]-i;
+       ans+=count[curr];
+       count[curr]++;
+   }
+   
+   cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
