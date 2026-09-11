43b3ba685cbc25249ca7784cf2248cc9848a11fe
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:11:21 +0530

    Add Bracket Coloring [D] from Codeforces

---
+++ 1837/D - Bracket Coloring/solution.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+#define PB push_back
+#define F first
+#define S second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+ 
+#define FOR(i,a,b) for(ll i=(a); i<(b); i++)
+#define RFOR(i,a,b) for(ll i=(a); i>=(b); i--)
+ 
+#define YES cout<<"Yes
+"
+#define NO cout<<"No
+"
+#define endl '
+'
+#define DEBUG(x) cerr<<#x<<":"<<x<<"
+";
+ 
+ 
+ll treeOperator(ll a,ll b);
+void buildSegmentTree(vll &tree,vll &nums,ll i,ll l,ll r);
+ll treeQuery(vll &tree,ll start,ll end,ll i,ll l,ll r);
+ 
+void solve()
+{
+   ll n;
+   cin>>n;
+   
+   string s;
+   cin>>s;
+   
+   vll ans(n,-1);
+   int count = 0;
+   
+   FOR(i,0,n){
+       if(s[i]=='('){
+           
+           if(count<0) ans[i]=2;
+           else ans[i] = 1;
+           
+           count++;
+       }
+       else{
+           if(count>0) ans[i]=1;
+           else ans[i]=2;
+           
+           count--;
+       }
+   }
+   
+   if(count!=0) cout<<-1;
+   else {
+       
+       bool one=false,two=false;
+       FOR(i,0,n){
+           if(ans[i]==1) one=true;
+           
+           else two=true;
+           
+           if(one&&two) break;
+       }
+       
+       cout<<(one+two)<<"
+";
+       if(!(one&&two)) FOR(i,0,n) cout<<1<<" ";
+       else FOR(i,0,n) cout<<ans[i]<<" ";
+   }
+   
+   cout<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
+ 
+ 
+void buildSegmentTree(vll &tree,vll &nums,ll i,ll l,ll r){
+    if(l==r){
+        tree[i] = nums[l];
+        return;
+    }
+    
+    ll mid = l + ((r-l)>>2);
+    buildSegmentTree(tree,nums,2*i+1,l,mid);
+    buildSegmentTree(tree,nums,2*i+2,mid+1,r);
+    
+    tree[i] = treeOperator(tree[2*i+1],tree[2*i+2]);
+}
+ 
+ll treeOperator(ll a,ll b){
+    return (a&b);    
+}
+ 
+ll treeQuery(vll &tree,ll start,ll end,ll i,ll l,ll r){
+    ll mid = l + ((r-l)>>1);
+    if(start>r || end<l) return 0;
+    else if(l<=start && r<=end) return tree[i];
+    else{
+        return treeOperator(treeQuery(tree,start,end,2*i+1,l,mid),
+            treeQuery(tree,start,end,2*i+2,mid+1,r)
+        );
+    }
+}
