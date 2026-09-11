be20117594bc12022d2bec90e153b5f95e9f6663
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:26:26 +0530

    Add Extremely Round [A] from Codeforces

---
+++ 1766/A - Extremely Round/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve()
+{
+    int n,ans=0;
+    cin>>n;
+    while(n/10!=0) {
+        ans+=9;
+        n/=10;
+    }
+    cout<<ans+n<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
