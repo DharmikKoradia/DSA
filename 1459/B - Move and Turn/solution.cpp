a95dc835a2f1e01a47b1ce7199e7239002a38865
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:08:34 +0530

    Add Move and Turn [B] from Codeforces

---
+++ 1459/B - Move and Turn/solution.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+#define PB push_back
+#define F first
+#define S second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+ 
+#define FOR(i,a,b) for(ll i=(a); i<(b); i++)
+#define RFOR(i,a,b) for(ll i=(a); i>=(b); i--)
+ 
+#define YES cout<<"Yes
+"
+#define NO cout<<"No
+"
+#define endl '
+'
+#define DEBUG(x) cerr<<#x<<x<<"
+";
+void solve()
+{
+    ll n;
+    cin>>n;
+    
+    ll odd = (n+1)/2;
+    ll even = n/2;
+    
+    if(n%2) cout<<(odd+1)*(even+1)*2;
+    else cout<<(odd+1)*(even+1);
+    
+    cout<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
