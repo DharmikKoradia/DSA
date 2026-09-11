1aaddc28b0dcb539fb670bb84e5a88e73c343199
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:39:01 +0530

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+ 
+void solve()
+{
+   ll n;
+   cin>>n;
+   for(int i = 1;i<=n;i++) cout<<i<<' ';
+   cout<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
