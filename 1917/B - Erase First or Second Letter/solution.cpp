0a436ab65f773bad7e371020b8a7af55cb2ade17
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:33:39 +0530

    Add Erase First or Second Letter [B] from Codeforces

---
+++ 1917/B - Erase First or Second Letter/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve()
+{
+    ll n,ans=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    unordered_set<char> temp;
+    for(auto it: s){
+        temp.insert(it);
+        ans+=temp.size();
+    }
+    cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
