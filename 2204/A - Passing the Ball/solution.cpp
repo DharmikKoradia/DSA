84cfae1f122c2d9cb82a09e1c227f6165b986a37
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:04:09 +0530

    Add Passing the Ball [A] from Codeforces

---
+++ 2204/A - Passing the Ball/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define SORT(x) sort(x.begin(),x.end())
+#define RSORT(x) sort(x.rbegin(),x.rend())
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+#define V(x,n) vector<ll> x(n);
+ 
+void solve()
+{
+    ll n;
+    string s;
+    cin>>n>>s;
+    
+    int i=0;
+    for(i=0;i<n;i++){
+        if(s[i]=='L') break;
+    }
+    
+    cout<<i+1<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
