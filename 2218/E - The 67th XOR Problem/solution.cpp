d5ffa0fb0ea2466b637ee0147b7dfd9d8fb3bc97
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:05:51 +0530

    Add The 67th XOR Problem [E] from Codeforces

---
+++ 2218/E - The 67th XOR Problem/solution.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define SORT(x) sort(x.begin(),x.end())
+#define RSORT(x) sort(x.rbegin(),x.rend())
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+#define V(x,n) vector<ll> x(n);
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    
+    V(nums,n);
+    FOR(i,0,n) cin>>nums[i];
+    
+    ll ans=0;
+    FOR(i,0,n){
+        FOR(j,i+1,n){
+            ans = max(ans,(nums[i]^nums[j]));
+        }
+    }
+    
+    cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
