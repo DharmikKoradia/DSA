ebdba72af829f717fd96c93a441cb1b7238fe2bb
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:36:31 +0530

    Add Soft Drinking [A] from Codeforces

---
+++ 151/A - Soft Drinking/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+ 
+void solve()
+{
+    int n, k, l, c, d, p, nl, np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    
+    cout<<min({(k*l)/nl,c*d,p/np})/n<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
