7e27d972a3e7b19c477752ea8c2bf996c75a2c54
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:29:21 +0530

    Add Three Indices [A] from Codeforces

---
+++ 1380/A - Three Indices/solution.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll> nums(n);
+    for(int i=0;i<n;i++) cin>>nums[i];
+    int temp=1;
+    while(temp<n && nums[temp-1]>nums[temp]) temp++;
+    if(temp==n || temp==n-1){
+        cout<<"No
+";
+        return;
+    }
+    ll lower=temp;
+    while(temp<n-1 && nums[temp+1]>nums[temp]) temp++;
+    if(temp==n-1){
+        cout<<"No
+";
+        return;
+    }
+    cout<<"Yes
+";
+    cout<<lower<<" "<<temp+1<<" "<<temp+2<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
