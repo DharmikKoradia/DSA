582fb64867519e304b2deeadbeb4e5a4bf1a9ce2
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:10:07 +0530

    Add Iskander and Drawings [A] from Codeforces

---
+++ 2244/A - Iskander and Drawings/solution.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+#define PB push_back
+#define F first
+#define S second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+ 
+#define FOR(i,a,b) for(ll i=(a); i<(b); i++)
+#define RFOR(i,a,b) for(ll i=(a); i>=(b); i--)
+ 
+#define YES cout<<"Yes
+"
+#define NO cout<<"No
+"
+#define endl '
+'
+#define DEBUG(x) cerr<<#x<<":"<<x<<"
+";
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    
+    ll c = 0,ans=0;
+    
+    for(int i=0;i<n;i++){
+        if(s[i]=='#') c++;
+        else{
+            ans=max(ans,c);
+            c=0;
+        }
+    }
+    ans = max(ans,c);
+    
+    cout<<(ans+1)/2<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
