1f1e62a0d851dc6d1829b81c5ef4615070d748d6
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:29:06 +0530

    Add Bad Boy [B] from Codeforces

---
+++ 1537/B - Bad Boy/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+void solve(){
+    long long x,y,sx,sy;
+    cin>>x>>y>>sx>>sy;
+    cout<<"1 1 "<<x<<" "<<y<<"
+";
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
