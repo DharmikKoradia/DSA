c0fb1281521c9036d1456872b232d9d2a77dd342
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:05:21 +0530

    Add Rock and Lever [B] from Codeforces

---
+++ 1420/B - Rock and Lever/solution.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define SORT(x) sort(x.begin(),x.end())
+#define RSORT(x) sort(x.rbegin(),x.rend())
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+#define V(x,n) vector<ll> x(n);
+ 
+void solve()
+{
+   ll n,ans=0;
+   vector<ll> nums(31,0);
+   cin>>n;
+   FOR(i,0,n){
+       ll x;
+       cin>>x;
+       ll t = log2(x);
+       nums[t]++;
+   }
+   FOR(i,0,31){
+       ans+= (nums[i]*(nums[i]-1))>>1;
+   }
+   
+   cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
