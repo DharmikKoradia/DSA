a19d6ec34c02c0993fcd7bba970acf68a9980b7f
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:23:18 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    if((n+1)%3==0 || (n-1)%3==0) cout<<"First
+";
+    else cout<<"Second
+";
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
