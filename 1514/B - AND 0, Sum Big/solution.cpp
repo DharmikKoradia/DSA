cf4eef6f0afdffaac7efb40feca13fc0b01a7ac4
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:04:21 +0530

    Add AND 0, Sum Big [B] from Codeforces

---
+++ 1514/B - AND 0, Sum Big/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define SORT(x) sort(x.begin(),x.end())
+#define RSORT(x) sort(x.rbegin(),x.rend())
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+#define V(x,n) vector<ll> x(n);
+ 
+const int MOD = 1e9+7;
+void solve()
+{
+    ll n,k;
+    ll ans=1;
+    cin>>n>>k;
+    
+    FOR(i,0,k){
+        ans = (ans*n)%MOD;
+    }
+    
+    cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
