3f2fa905d7d6d32484e0d67ddbf61ff4b569aaf9
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:11:40 +0530

    Add Hossam and Friends [B] from Codeforces

---
+++ 1771/B - Hossam and Friends/solution.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ 
+#define PB push_back
+#define F first
+#define S second
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+ 
+#define FOR(i,a,b) for(ll i=(a); i<(b); i++)
+#define RFOR(i,a,b) for(ll i=(a); i>=(b); i--)
+ 
+#define YES cout<<"Yes
+"
+#define NO cout<<"No
+"
+#define endl '
+'
+#define DEBUG(x) cerr<<#x<<":"<<x<<"
+";
+ 
+ 
+ll treeOperator(ll a,ll b);
+void buildSegmentTree(vll &tree,vll &nums,ll i,ll l,ll r);
+ll treeQuery(vll &tree,ll start,ll end,ll i,ll l,ll r);
+ 
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+    
+    vector<ll> conflict(n+1,n+1);
+    
+    FOR(i,0,m){
+        ll x,y;
+        cin>>x>>y;
+        
+        int temp = max(x,y);
+        x = min(x,y);
+        y = temp;
+        
+        conflict[x] = min(conflict[x],y);
+    }
+    
+    for (int i = n - 1; i >= 1; i--)
+        conflict[i] = min(conflict[i], conflict[i + 1]);
+    
+    ll ans=0;
+    FOR(i,1,n+1){
+        // cout<<conflict[i]<<" ";
+        ans+=conflict[i]-i;
+    }
+    
+    cout<<ans<<"
+";
+    
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
+ 
+ 
+void buildSegmentTree(vll &tree,vll &nums,ll i,ll l,ll r){
+    if(l==r){
+        tree[i] = nums[l];
+        return;
+    }
+    
+    ll mid = l + ((r-l)>>1);
+    buildSegmentTree(tree,nums,2*i+1,l,mid);
+    buildSegmentTree(tree,nums,2*i+2,mid+1,r);
+    
+    tree[i] = treeOperator(tree[2*i+1],tree[2*i+2]);
+}
+ 
+ll treeOperator(ll a,ll b){
+    return (a&b);    
+}
+ 
+ll treeQuery(vll &tree,ll start,ll end,ll i,ll l,ll r){
+    ll mid = l + ((r-l)>>1);
+    if(start>r || end<l) return 0;
+    else if(l<=start && r<=end) return tree[i];
+    else{
+        return treeOperator(treeQuery(tree,start,end,2*i+1,l,mid),
+            treeQuery(tree,start,end,2*i+2,mid+1,r)
+        );
+    }
+}//
