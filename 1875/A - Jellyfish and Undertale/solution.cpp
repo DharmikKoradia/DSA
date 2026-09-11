9bf87dc6bf09fc807ddd7bf789d0401403d4f483
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:26:52 +0530

    Add Jellyfish and Undertale [A] from Codeforces

---
+++ 1875/A - Jellyfish and Undertale/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve()
+{
+    ll a,b,n,ans;
+    cin>>a>>b>>n;
+    ans=b;
+    for(int i=0;i<n;i++){
+        ll x;
+        cin>>x;
+        ans+=min(x,a-1);
+    }
+    cout<<ans<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
