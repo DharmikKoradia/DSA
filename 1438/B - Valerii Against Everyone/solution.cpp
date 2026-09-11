aa4aa8817041cacd214f8ba97dfd132512aea816
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:33:12 +0530

    Add Valerii Against Everyone [B] from Codeforces

---
+++ 1438/B - Valerii Against Everyone/solution.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    unordered_set<int> temp;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        temp.insert(x);
+    }
+    temp.size()==n? cout<<"No
+":cout<<"Yes
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
