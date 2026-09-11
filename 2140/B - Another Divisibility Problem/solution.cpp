1740adb5699f178e79b8824ee858817242df9f87
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:35:09 +0530

    Add Another Divisibility Problem [B] from Codeforces

---
+++ 2140/B - Another Divisibility Problem/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+ 
+ 
+void solve()
+{
+   ll x;
+   cin>>x;
+   cout<<2*x<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
