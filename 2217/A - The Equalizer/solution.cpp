a2f528df960503219fb3b31a716c7f99e7d088d2
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 22:05:56 +0530

    Add The Equalizer [A] from Codeforces

---
+++ 2217/A - The Equalizer/solution.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(ll i=a;i<b;i++)
+#define SORT(x) sort(x.begin(),x.end())
+#define RSORT(x) sort(x.rbegin(),x.rend())
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+#define V(x,n) vector<ll> x(n);
+ 
+void solve()
+{
+   ll n,k,sum=0;
+   cin>>n>>k;
+   for(int i=0;i<n;i++){
+       ll x;
+       cin>>x;
+       sum+=x;
+   }
+   if(sum%2==1) cout<<"Yes
+";
+   else if(((n*k)-1)%2==1){
+       cout<<"Yes
+";
+   }
+   else cout<<"No
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
