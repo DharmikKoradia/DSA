1f91e756d39ce71053474cd8a13ccef0303f0751
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:30:48 +0530

    Add Shizuku Hoshikawa and Farm Legs [A] from Codeforces

---
+++ 2171/A - Shizuku Hoshikawa and Farm Legs/solution.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve()
+{
+    int n;
+    cin>>n;
+    if(n==0){
+        cout<<"0
+";
+        return;
+    }
+    if(n%2){
+        cout<<"0
+";
+        return;
+    }
+    cout<<n/4+1<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
