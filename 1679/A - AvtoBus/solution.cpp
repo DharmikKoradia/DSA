36a6164940299e0aa55e76f52cefbd8500e50139
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:27:35 +0530

    Add AvtoBus [A] from Codeforces

---
+++ 1679/A - AvtoBus/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    if(n%2 || n<4) cout<<"-1
+";
+    else cout<<(n+5)/6<<" "<<n/4<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
