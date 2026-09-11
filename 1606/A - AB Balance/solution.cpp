55788cf34e5e2bcb989cb57bdf2f0f1a6c574ab8
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:28:45 +0530

    Add AB Balance [A] from Codeforces

---
+++ 1606/A - AB Balance/solution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+ 
+void solve()
+{
+    string s;
+    cin>>s;
+    int n=s.size();
+    if(s[0]!=s[n-1]) s[0]=s[n-1];
+    cout<<s<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
+ 
