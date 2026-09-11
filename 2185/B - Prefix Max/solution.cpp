8ff2d75de1a91f658dbc9882aa1241cee7cb50c9
DharmikKoradia <dharmik.koradia@gmail.com>
2026-09-11 18:39:05 +0530

    Add Prefix Max [B] from Codeforces

---
+++ 2185/B - Prefix Max/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+//Macro definition
+typedef long long ll;
+typedef vector<int> vi;
+#define PB push_back
+#define POP pop_back()
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define DEBUG(x) cerr<<#x<<" = "<<x;
+ 
+void solve()
+{
+   ll n,ans=0;
+   cin>>n;
+   for(int i = 0;i<n;i++){
+       ll x;
+       cin>>x;
+       ans=max(ans,x);
+   }
+   cout<<ans*n<<"
+";
+}
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+	    solve();
+    }
+}
